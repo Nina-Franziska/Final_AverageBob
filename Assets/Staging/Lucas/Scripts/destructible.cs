57f1732a1e3e9ae9b20d7a6b9f8518af9ab3e113
Nina-Franziska Christiansen <nina-franziska@hotmail.com>
2020-11-15 22:04:41 +0100

    changed the destructable script back + made a destructableCollide script for throw objects

--- Assets/Staging/Lucas/Scripts/destructible.cs
+++ Assets/Staging/Lucas/Scripts/destructible.cs
@@ -7,24 +7,16 @@ public class destructible : MonoBehaviour
 {
     public GameObject destroyedVersion;
 
-    private void Start()
+
+
+
+    private void OnMouseDown()
     {
-        
+        Instantiate(destroyedVersion, transform.position, transform.rotation);
+        Destroy(gameObject);
+
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-
-        if (collision.relativeVelocity.magnitude > 2)
-        {
-            Instantiate(destroyedVersion, transform.position, transform.rotation);
-            Destroy(gameObject);
-
-        }
-          
-    
-
-    }
 
 
 
