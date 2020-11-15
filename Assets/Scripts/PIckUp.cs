ccf934bd2e4cec0a6ba839019202ce3e4af4a475
Nina-Franziska Christiansen <nina-franziska@hotmail.com>
2020-11-15 18:40:51 +0100

    First upload of all files

---
+++ Assets/Scripts/PIckUp.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PIckUp : MonoBehaviour
+{
+    [SerializeField]
+    Transform hand;
+
+    bool handFree = true;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IsItGrabbable();
+
+    }
+
+
+
+
+
+    void IsItGrabbable()
+    {
+
+
+        GameObject[] grabObjArray = GameObject.FindGameObjectsWithTag("Grab");
+    
+        for(int i = 0; i < grabObjArray.Length; i++) {
+
+        float dist =  Vector3.Distance(transform.position, grabObjArray[i].transform.position);
+
+            if (dist < 0.8)
+            {
+
+                Grab(grabObjArray[i]);
+
+  
+
+
+            }
+
+        }
+
+
+    }
+
+
+
+    void Grab(GameObject grabObj)
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space) && handFree == true)
+        {
+
+            handFree = false;
+
+            grabObj.GetComponent<Collider>().enabled = false;
+            grabObj.GetComponent<Rigidbody>().isKinematic = true;
+            grabObj.GetComponent<Rigidbody>().useGravity = false;
+            grabObj.transform.position = hand.position;
+            grabObj.transform.parent = hand;
+
+
+
+        }
+
+        if (handFree == false)
+        {
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+
+                print("throw");
+
+                grabObj.GetComponent<Collider>().enabled = true;
+                grabObj.GetComponent<Rigidbody>().isKinematic = false;
+                grabObj.GetComponent<Rigidbody>().useGravity = true;
+                grabObj.transform.parent = null;
+                handFree = true;
+
+
+            }
+
+
+
+        }
+
+
+    }
+
+}
---
+++ Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+
+[SerializeField]
+float speed = 1.0f;
+
+[SerializeField]
+Rigidbody rb;
+
+
+        public Vector3 jump;
+         public float jumpForce = 2.0f;
+     
+         public bool isGrounded;
+         void Start(){
+      
+             jump = new Vector3(0.0f, 2.0f, 0.0f);
+         }
+     
+         void OnCollisionStay()
+         {
+             isGrounded = true;
+             Debug.Log("er grounded");
+         }
+     
+         void Update(){
+             if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
+     
+                 rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+                 isGrounded = false;
+                 Debug.Log("hop");
+             }
+         }
+    void FixedUpdate()
+    {
+        // Get the horizontal and vertical axis.
+        // By default they are mapped to the arrow keys.
+        // The value is in the range -1 to 1
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * speed;
+
+        // Move translation along he object's z-axis
+        transform.Translate(0, 0, translation);
+        transform.Translate(rotation, 0, translation);
+
+        // Rotate around our y-axis
+       // transform.Rotate(0, rotation, 0);
+
+       
+    }
+}
+
